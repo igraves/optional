84c88e583c7aab445feff0234a34f710c8599920
Ian Graves <ian.graves@equipmentshare.com>
2019-11-24 21:43:18 -0600

    Updates to cabal 3

--- Setup.hs
+++ Setup.hs
@@ -2,18 +2,17 @@
 module Main (main) where
 
 import Data.List ( nub )
-import Distribution.Types.Version( showVersion )
-import Distribution.Types.PackageName (PackageName, unPackageName)
+--import Distribution.Types.Version( showVersion )
+import Distribution.Pretty (prettyShow)
+import Distribution.Types.PackageName (unPackageName)
 import Distribution.Types.UnqualComponentName (unUnqualComponentName)
 import Distribution.Types.UnitId (UnitId)
 import Distribution.Types.MungedPackageId (MungedPackageId(mungedName, mungedVersion))
-import Distribution.Types.MungedPackageName (decodeCompatPackageName)
-
-import Distribution.Package (PackageId, InstalledPackageId, packageVersion, packageName )
+import Distribution.Types.MungedPackageName (encodeCompatPackageName)
 import Distribution.PackageDescription ( PackageDescription(), TestSuite(..) )
 import Distribution.Simple ( defaultMainWithHooks, UserHooks(..), simpleUserHooks )
-import Distribution.Simple.Utils ( rewriteFile, createDirectoryIfMissingVerbose )
-import Distribution.Simple.BuildPaths ( autogenModulesDir )
+import Distribution.Simple.Utils ( rewriteFileEx, createDirectoryIfMissingVerbose )
+import Distribution.Simple.BuildPaths ( autogenPackageModulesDir )
 import Distribution.Simple.Setup ( BuildFlags(buildVerbosity), fromFlag )
 import Distribution.Simple.LocalBuildInfo ( withLibLBI, withTestLBI, LocalBuildInfo(), ComponentLocalBuildInfo(componentPackageDeps) )
 import Distribution.Verbosity ( Verbosity )
@@ -28,20 +27,20 @@ main = defaultMainWithHooks simpleUserHooks
 
 generateBuildModule :: Verbosity -> PackageDescription -> LocalBuildInfo -> IO ()
 generateBuildModule verbosity pkg lbi = do
-  let dir = autogenModulesDir lbi
+  let dir = autogenPackageModulesDir lbi
   createDirectoryIfMissingVerbose verbosity True dir
   withLibLBI pkg lbi $ \_ libcfg -> do
     withTestLBI pkg lbi $ \suite suitecfg -> do
       let testNameSuite = unUnqualComponentName $ testName suite
-      rewriteFile (dir </> "Build_" ++ testNameSuite ++ ".hs") $ unlines
+      rewriteFileEx verbosity (dir </> "Build_" ++ testNameSuite ++ ".hs") $ unlines
         [ "module Build_" ++ testNameSuite ++ " where"
         , "deps :: [String]"
         , "deps = " ++ (show $ formatdeps (testDeps libcfg suitecfg))
         ]
   where
     formatdeps = map (formatone . snd)
-    formatone p = let n = unPackageName  (fst $ decodeCompatPackageName $ mungedName p) 
-                   in n ++ "-" ++ showVersion (mungedVersion p)
+    formatone p = let n = unPackageName  (encodeCompatPackageName $ mungedName p) 
+                   in n ++ "-" ++ prettyShow (mungedVersion p)
 
 testDeps :: ComponentLocalBuildInfo -> ComponentLocalBuildInfo -> [(UnitId, MungedPackageId)]
 testDeps xs ys = nub $ componentPackageDeps xs ++ componentPackageDeps ys
